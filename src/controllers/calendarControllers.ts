d38d4aef90c9b83171f31a61152fa2e306c42fdd
Ahsan4k <83755681+Ahsan4k@users.noreply.github.com>
2024-02-23 17:06:14 -0600

    changes

--- src/controllers/calendarControllers.ts
+++ src/controllers/calendarControllers.ts
@@ -4,33 +4,18 @@ import { Signup } from "../models/signup";
 
 export const SetDate = async (req, res) => {
   const dates = req.body;
-  // console.log("body=========>", dates);
-  const filter = {
-    date: dates.date,
-    startDate: dates.startDate,
-    endDate: dates.endDate,
-  };
+  console.log("dates========>", dates);
   const exist = await Signup.find({ token: dates.token });
-  const recordExist = await Booking.find(filter);
-
   if (exist.length > 0) {
-    if (recordExist.length > 0 && recordExist[0]?.count < 2) {
-      const update = {
-        count: dates.count + recordExist[0]?.count,
-      };
-      const val = await Booking.findOneAndUpdate(filter, update);
-    } else {
-      const insertData = new Booking({
-        type: dates.type,
-        startDate: dates.startDate,
-        endDate: dates.endDate,
-        date: dates.date,
-        token: dates.token,
-        count: dates.count,
-      });
-      await insertData.save();
-      res.json({ status: "Success" });
-    }
+    const insertData = new Booking({
+      type: dates.type,
+      startTime: dates.startTime,
+      endTime: dates.endTime,
+      date: dates.date,
+      token: dates.token,
+    });
+    await insertData.save();
+    res.json({ status: "Success" });
   } else {
     res.json({ status: "User does not exist" });
   }
--- src/models/booking.ts
+++ src/models/booking.ts
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 const BookingSchema = new mongoose.Schema({
   type: String,
-  startDate: Date,
-  endDate: Date,
+  startTime: String,
+  endTime: String,
   date: String,
   token: String,
-  count: Number,
 });
 
 export const Booking = mongoose.model("Booking", BookingSchema);
